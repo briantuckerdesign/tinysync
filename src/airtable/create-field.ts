8dd0f083a284eb0753f75c91224f002a4489df04
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-11 13:48:01 -0500

    Update create-field.ts

--- src/airtable/create-field.ts
+++ src/airtable/create-field.ts
@@ -1,31 +1,41 @@
-import { AirtableField } from "../types/airtable";
-import { ui } from "../ui";
+import { AirtableField } from '../types/airtable'
+import { ui } from '../ui'
 
-export async function createField(token: string, baseId: string, tableId: string, field: AirtableField): Promise<AirtableField> {
-    const url = `https://api.airtable.com/v0/meta/bases/${baseId}/tables/${tableId}/fields`;
+export async function createField(
+    token: string,
+    baseId: string,
+    tableId: string,
+    field: AirtableField
+): Promise<AirtableField> {
+    const url = `https://api.airtable.com/v0/meta/bases/${baseId}/tables/${tableId}/fields`
     try {
         const response = await fetch(url, {
-            method: "POST",
+            method: 'POST',
             headers: {
                 Authorization: `Bearer ${token}`,
-                "Content-Type": "application/json",
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify(field),
-        });
+        })
 
         if (!response.ok) {
-            const errorText = await response.text();
-            throw new Error(`HTTP error! status: ${response.status}, message: ${errorText}`);
+            const errorText = await response.text()
+            throw new Error(
+                `HTTP error! status: ${response.status}, message: ${errorText}`
+            )
         }
 
-        const createdField = await response.json();
+        const createdField = await response.json()
 
-        if (!createdField.id) throw new Error("Invalid response: created field is missing id property");
+        if (!createdField.id)
+            throw new Error(
+                'Invalid response: created field is missing id property'
+            )
 
-        return createdField as AirtableField;
+        return createdField as AirtableField
     } catch (error) {
-        ui.prompt.log.error("Error creating field.");
-        ui.prompt.log.error(error as string);
-        process.exit(0);
+        ui.prompt.log.error('Error creating field.')
+        ui.prompt.log.error(error as string)
+        process.exit(0)
     }
 }
