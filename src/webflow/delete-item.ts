79f00a7afefb73cb174b63b495a4a1efd8ca738e
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-11 17:43:07 -0500

    Create delete-item.ts

---
+++ src/webflow/delete-item.ts
@@ -0,0 +1,34 @@
+import { ui } from '../ui'
+
+export async function deleteItem(
+    token: string,
+    collectionId: string,
+    itemId: string
+): Promise<boolean> {
+    try {
+        const url = `https://api.webflow.com/v2/collections/${collectionId}/items/${itemId}`
+
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        })
+
+        if (!response.ok && response.status !== 204) {
+            const errorText = await response.text()
+            throw new Error(
+                `HTTP error! status: ${response.status}, message: ${errorText}`
+            )
+        }
+
+        if (response.status === 204) return true
+
+        return false
+    } catch (error) {
+        ui.prompt.log.error('Error deleting item.')
+        throw error
+    }
+}
