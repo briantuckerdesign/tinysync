cc79412d66ce5719380f86a833e19b5c4ea7c47d
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-11 14:15:00 -0500

    Update get-schema.ts

--- src/airtable/get-schema.ts
+++ src/airtable/get-schema.ts
@@ -1,42 +1,53 @@
-import axios from "axios";
-import { ui } from "../ui";
+import type { AirtableTable } from '../types/airtable'
+import { ui } from '../ui'
 
-export async function getUpdatedSchema(syncConfig: Sync) {
-  const apiKey = syncConfig.airtable.accessToken;
-  const baseId = syncConfig.airtable.base.id;
-  const tableId = syncConfig.airtable.table.id;
-  const viewId = syncConfig.airtable.table.view.id;
-  const url = `https://api.airtable.com/v0/meta/bases/${baseId}/tables`;
-  const options = {
-    headers: {
-      Authorization: `Bearer ${apiKey}`,
-    },
-  };
+export async function getSchema(
+    token: string,
+    viewId: string,
+    tableId: string,
+    baseId: string
+) {
+    const url = `https://api.airtable.com/v0/meta/bases/${baseId}/tables`
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        })
 
-  try {
-    // Docs: https://airtable.com/developers/web/api/get-base-schema
-    const response = await axios.get(url, options);
+        if (!response.ok) {
+            const errorText = await response.text()
+            throw new Error(
+                `HTTP error! status: ${response.status}, message: ${errorText}`
+            )
+        }
 
-    const tables = response.data.tables;
-    const table = tables.find((table) => {
-      return table.id === tableId;
-    });
-    if (!table) {
-      throw new Error("Table not found. It may have been deleted.");
+        const data: any = await response.json()
+
+        if (!data.tables || !Array.isArray(data.tables))
+            throw new Error('Invalid response from Airtable API')
+
+        const tables: AirtableTable[] = data.tables
+        const table = tables.find((table) => {
+            return table.id === tableId
+        })
+
+        if (!table)
+            throw new Error('Table not found. It may have been deleted.')
+
+        const views = table.views
+        const view = views.find((view) => {
+            return view.id === viewId
+        })
+
+        if (!view) throw new Error('View not found. It may have been deleted.')
+
+        return table
+    } catch (error) {
+        ui.prompt.log.error('Error getting Airtable schema:')
+        ui.prompt.log.error(error as string)
+        process.exit(0)
     }
-
-    const views = table.views;
-    const view = views.find((view) => {
-      return view.id === viewId;
-    });
-    if (!view) {
-      throw new Error("View not found. It may have been deleted.");
-    }
-
-    return table;
-  } catch (error) {
-    ui.prompt.log.error("Error getting Airtable schema:");
-    ui.prompt.log.error(error);
-    process.exit(0);
-  }
 }
