75990e0225d0b0640217ee72280abc915b500f2d
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-11 18:19:49 -0500

    Update get-items.ts

--- src/webflow/get-items.ts
+++ src/webflow/get-items.ts
@@ -1,57 +1,52 @@
-/* -------------------------------------------------------------------------- */
-/*                             Webflow / Get items                            */
-/* -------------------------------------------------------------------------- */
-/**
- * Fetches records from Webflow using the provided configuration.
- * @param {Object} syncConfig - The configuration object.
- * @param {Object} selectedSync.webflow - The Webflow configuration.
- * @param {string} config.webflow.collectionId - The ID of the Webflow collection.
- * @param {string} config.webflow.apiKey - The API key for accessing the Webflow API.
- * @returns {Promise<Array>} - A promise that resolves to an array of Webflow records.
- * @throws {Error} - If there is an error fetching the Webflow items.
- */
+import type { CollectionItem, CollectionItemList } from 'webflow-api/api'
+import { ui } from '../ui'
 
-import axios from "axios";
-import { ui } from "../ui";
+export async function getItems(
+    token: string,
+    collectionId: string
+): Promise<CollectionItem[]> {
+    try {
+        let allItems: CollectionItem[] = []
+        let offset = 0
+        let total = 0
+        let firstRun = true
+        const baseUrl = `https://api.webflow.com/v2/collections/${collectionId}/items`
 
-export async function getItems(syncConfig: Sync) {
-  try {
-    const collectionId = syncConfig.webflow.collection.id;
-    const apiKey = syncConfig.webflow.accessToken;
-    let allItems = [];
-    let offset = 0;
-    let total = 0;
-    let firstRun = true;
-    const url = `https://api.webflow.com/v2/collections/${collectionId}/items`;
-    const options = {
-      headers: {
-        accept: "application/json",
-        authorization: `Bearer ${apiKey}`,
-      },
-      params: {
-        limit: 100,
-        offset: offset,
-      },
-    };
+        do {
+            const url = `${baseUrl}?limit=100&offset=${offset}`
+            const response = await fetch(url, {
+                headers: {
+                    accept: 'application/json',
+                    authorization: `Bearer ${token}`,
+                },
+            })
 
-    do {
-      const response = await axios.get(url, options);
+            if (!response.ok) throw new Error(`HTTP error: ${response.status}`)
 
-      const items = response.data.items;
-      const pagination = response.data.pagination;
-      allItems.push(...items);
+            const data: any = await response.json()
 
-      if (firstRun) {
-        total = pagination.total;
-        firstRun = false;
-      }
+            const itemList: CollectionItemList = data
+            if (!itemList.items) throw new Error('No items found')
+            if (!itemList.pagination) throw new Error('No pagination found')
 
-      offset += items.length;
-    } while (offset < total);
+            const items = itemList.items
+            const pagination = itemList.pagination
 
-    return allItems;
-  } catch (error) {
-    ui.prompt.log.error("Error getting items.");
-    throw error;
-  }
+            allItems.push(...items)
+
+            if (firstRun) {
+                if (!pagination.total)
+                    throw new Error('No pagination total found')
+                total = pagination.total
+                firstRun = false
+            }
+
+            offset += items.length
+        } while (offset < total)
+
+        return allItems
+    } catch (error) {
+        ui.prompt.log.error('Error getting items.')
+        throw error
+    }
 }
