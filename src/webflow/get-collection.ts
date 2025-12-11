02c25606207f7518695f3c64278f5efa56325782
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-11 17:49:47 -0500

    Create get-collection.ts

---
+++ src/webflow/get-collection.ts
@@ -0,0 +1,33 @@
+import type { Collection } from 'webflow-api/api'
+import { ui } from '../ui'
+
+export async function getCollection(
+    token: string,
+    collectionId: string
+): Promise<Collection> {
+    const url = `https://api.webflow.com/v2/collections/${collectionId}`
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        })
+
+        if (!response.ok && response.status !== 200) {
+            const errorText = await response.text()
+            throw new Error(
+                `HTTP error! status: ${response.status}, message: ${errorText}`
+            )
+        }
+
+        const data = await response.json()
+
+        return data as Collection
+    } catch (error) {
+        ui.prompt.log.error('Error getting collection.')
+        throw error
+    }
+}
