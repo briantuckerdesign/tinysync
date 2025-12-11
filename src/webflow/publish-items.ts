f51f89f1c92cee0efe4f4408bfe766f0f48ec6b1
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-11 18:09:10 -0500

    Create publish-items.ts

---
+++ src/webflow/publish-items.ts
@@ -0,0 +1,43 @@
+import type { PublishedWebflowItems } from '../types/webflow'
+import { ui } from '../ui'
+
+export async function publishItems(
+    token: string,
+    collectionId: string,
+    itemIds: string[]
+) {
+    const url = `https://api.webflow.com/v2/collections/${collectionId}/items/publish`
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                accept: 'application/json',
+                authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ itemIds: itemIds }),
+        })
+
+        if (!response.ok) {
+            const errorText = await response.text()
+            throw new Error(
+                `HTTP error! status: ${response.status}, message: ${errorText}`
+            )
+        }
+
+        if (!response.ok || response.status != 202) {
+            const errorText = await response.text()
+            throw new Error(
+                `HTTP error! status: ${response.status}, message: ${errorText}`
+            )
+        }
+
+        const data: any = await response.json()
+        const publishedItems: PublishedWebflowItems = data
+
+        return publishedItems
+    } catch (error) {
+        ui.prompt.log.error('Error publishing items.')
+        throw error
+    }
+}
