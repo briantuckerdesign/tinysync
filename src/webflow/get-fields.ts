3736dc70078340baa86ccf5ce5b128036c175a61
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-11 17:53:24 -0500

    Create get-fields.ts

---
+++ src/webflow/get-fields.ts
@@ -0,0 +1,35 @@
+import type { Field } from 'webflow-api/api'
+import { ui } from '../ui'
+
+export async function getFields(
+    token: string,
+    collectionId: string
+): Promise<Field[]> {
+    const url = `https://api.webflow.com/v2/collections/${collectionId}`
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+        })
+
+        if (!response.ok) {
+            const errorText = await response.text()
+            throw new Error(
+                `HTTP error! status: ${response.status}, message: ${errorText}`
+            )
+        }
+
+        const data: any = await response.json()
+
+        const fields: Field[] = data.fields
+
+        return fields
+    } catch (error) {
+        ui.prompt.log.error('Error getting fields.')
+        throw error
+    }
+}
