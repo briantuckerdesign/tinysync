5ae096009c8df249f0ff31cdbe77ef79fb455552
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-11 14:16:56 -0500

    Update get-tables.ts

--- src/airtable/get-tables.ts
+++ src/airtable/get-tables.ts
@@ -1,36 +1,43 @@
-import { AirtableTable } from "../types/airtable";
-import { ui } from "../ui";
+import type { AirtableTable } from '../types/airtable'
+import { ui } from '../ui'
 
-export async function getTables(token: string, baseId: string): Promise<AirtableTable[]> {
-    const url = `https://api.airtable.com/v0/meta/bases/${baseId}/tables`;
+export async function getTables(
+    token: string,
+    baseId: string
+): Promise<AirtableTable[]> {
+    const url = `https://api.airtable.com/v0/meta/bases/${baseId}/tables`
     try {
         const response = await fetch(url, {
-            method: "GET",
+            method: 'GET',
             headers: {
                 Authorization: `Bearer ${token}`,
-                "Content-Type": "application/json",
+                'Content-Type': 'application/json',
             },
-        });
+        })
 
         if (!response.ok) {
-            const errorText = await response.text();
-            throw new Error(`HTTP error! status: ${response.status}, message: ${errorText}`);
+            const errorText = await response.text()
+            throw new Error(
+                `HTTP error! status: ${response.status}, message: ${errorText}`
+            )
         }
 
-        const data = await response.json();
+        const data: any = await response.json()
 
-        if (!Array.isArray(data.tables)) throw new Error("Invalid response: tables is not an array");
+        if (!data.tables || !Array.isArray(data.tables))
+            throw new Error('Invalid response: tables is not an array')
 
-        const tables: any[] = data.tables;
+        const tables: any[] = data.tables
 
         tables.forEach((table) => {
-            if (!table.name) throw new Error("Invalid table: missing name property");
-        });
+            if (!table.name)
+                throw new Error('Invalid table: missing name property')
+        })
 
-        return tables as AirtableTable[];
+        return tables as AirtableTable[]
     } catch (error) {
-        ui.prompt.log.error("Error getting tables.");
-        ui.prompt.log.error(error as string);
-        process.exit(0);
+        ui.prompt.log.error('Error getting tables.')
+        ui.prompt.log.error(error as string)
+        process.exit(0)
     }
 }
