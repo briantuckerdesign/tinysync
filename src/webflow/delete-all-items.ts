4e418cbd3c7140545c22ebc2fff95c4fc23665cb
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-11 18:26:26 -0500

    Update delete-all-items.ts

--- src/webflow/delete-all-items.ts
+++ src/webflow/delete-all-items.ts
@@ -1,27 +1,28 @@
-/* -------------------------------------------------------------------------- */
-/*                         Webflow / Delete all items                         */
-/* -------------------------------------------------------------------------- */
-/**
- * Deletes all items from a Webflow collection.
- *
- * @param {string} selectedSync - The selected sync, contains the Webflow API key and collection ID.
- * @returns {Promise<void>} - A promise that resolves when all items are deleted.
- */
+import { ui } from '../ui'
+import { deleteItems } from './delete-items'
+import { getItems } from './get-items'
 
-import { ui } from "../ui";
-import { webflow } from "./index";
+export async function deleteAllItems(
+    token: string,
+    collectionId: string
+): Promise<boolean> {
+    try {
+        const itemIds: string[] = []
+        const items = await getItems(token, collectionId)
 
-export async function deleteAllItems(selectedSync: Sync) {
-  try {
-    const items = await webflow.getItems(selectedSync);
-    for (let item of items) {
-      const itemId = item.id;
-      await webflow.deleteItem(itemId, selectedSync);
+        items.forEach((item) => {
+            if (item.id) itemIds.push(item.id)
+        })
+
+        try {
+            const deleted = await deleteItems(token, collectionId, itemIds)
+            return deleted
+        } catch (error) {
+            ui.prompt.log.error('Error deleting all items.')
+            throw error
+        }
+    } catch (error) {
+        ui.prompt.log.error('Error deleting all items.')
+        throw error
     }
-
-    return;
-  } catch (error) {
-    ui.prompt.log.error("Error deleting all items.");
-    throw error;
-  }
 }
