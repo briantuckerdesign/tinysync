288b0ac7bb3cd7f1671331e7bdbe56e760d5dc21
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-11 14:17:29 -0500

    Update index.ts

--- src/airtable/index.ts
+++ src/airtable/index.ts
@@ -1,23 +1,19 @@
-/**
- * My version of Airtable client since I can't seem to get the official one to work.
- */
-
-import { getBases } from "./get-bases";
-import { getTables } from "./get-tables";
-import { getRecords } from "./get-records";
-import { updateRecord } from "./update-record";
-import { createField } from "./create-field";
-import { getRecord } from "./get-record";
-import { getUpdatedSchema } from "./get-schema";
-import { updateRecords } from "./update-records";
+import { getBases } from './get-bases'
+import { getTables } from './get-tables'
+import { getRecords } from './get-records'
+import { updateRecord } from './update-record'
+import { createField } from './create-field'
+import { getRecord } from './get-record'
+import { getSchema } from './get-schema'
+import { updateRecords } from './update-records'
 
 export const airtable = {
-  getBases,
-  getTables,
-  getRecord,
-  getRecords,
-  getSchema: getUpdatedSchema,
-  updateRecord,
-  updateRecords,
-  createField,
-};
+    getBases,
+    getTables,
+    getRecord,
+    getRecords,
+    getSchema,
+    updateRecord,
+    updateRecords,
+    createField,
+}
