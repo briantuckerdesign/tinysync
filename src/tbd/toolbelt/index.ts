aad839785c0a130242d25868a2d1d2a76243108d
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-11 19:59:37 -0500

    Update index.ts

--- src/tbd/toolbelt/index.ts
+++ src/tbd/toolbelt/index.ts
@@ -1,14 +1,9 @@
-/* -------------------------------------------------------------------------- */
-/*                                    Utils                                   */
-/* -------------------------------------------------------------------------- */
 import { parseRecordData } from './parse-record-data'
-import { filterByPropertyPath } from './filter-by-property-path'
 import { findSpecial } from './find-special-field'
 import { encapsulateObjectForSelect } from './encapsulate-object-for-select'
 
 export const toolbelt = {
     encapsulateObjectForSelect,
     findSpecial,
-    filterByPropertyPath,
     parseRecordData,
 }
