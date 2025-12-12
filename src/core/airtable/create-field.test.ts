a65afff6340656dad8757396873a9a5b8317bdd7
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-12 11:26:04 -0500

    Add tests for Airtable core functions
    Introduced unit tests for Airtable core functions including createField, getBases, getRecord, getRecords, getSchema, getTables, and updateRecord. These tests use mock data to verify expected properties and behaviors, improving reliability and coverage of the Airtable integration.

---
+++ src/core/airtable/create-field.test.ts
@@ -0,0 +1,25 @@
+import { expect, test } from 'bun:test'
+import { mockData } from '../../../dev/mock-data'
+import { createField } from './create-field'
+import type { AirtableField, AirtableFieldType } from './types'
+
+const type: AirtableFieldType = 'singleLineText'
+const name = `delete-me-${Math.random().toString(36).substring(2, 6)}`
+const fieldToCreate: AirtableField = {
+    type,
+    name,
+}
+
+test('Airtable - createField', async () => {
+    const field = await createField(
+        mockData.tokens.airtable,
+        mockData.baseId,
+        mockData.tableId,
+        fieldToCreate
+    )
+    expect(field).toHaveProperty('id')
+    expect(field).toHaveProperty('name')
+    expect(field.name).toBe(name)
+    expect(field).toHaveProperty('type')
+    expect(field.type).toBe(type)
+})
