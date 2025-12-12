b84a9d57a22adee26b339dd6c53fb2f408167ea6
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-12 13:54:05 -0500

    removed toolbelt concept

--- src/tbd/toolbelt/find-special-field.ts
+++
@@ -1,3 +0,0 @@
-export function findSpecial(fieldName: SpecialField, syncConfig: Sync) {
-    return syncConfig.fields.find((field) => field.specialField === fieldName)
-}
--- src/tbd/toolbelt/index.ts
+++
@@ -1,9 +0,0 @@
-import { parseRecordData } from './parse-record-data'
-import { findSpecial } from './find-special-field'
-import { encapsulateObjectForSelect } from './encapsulate-object-for-select'
-
-export const toolbelt = {
-    encapsulateObjectForSelect,
-    findSpecial,
-    parseRecordData,
-}
