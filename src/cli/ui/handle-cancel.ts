6102bab6c428ca3229be5eff83f53df5aef006eb
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-13 11:44:06 -0500

    Update handle-cancel.ts

--- src/cli/ui/handle-cancel.ts
+++ src/cli/ui/handle-cancel.ts
@@ -1,13 +1,13 @@
 import { ui } from '.'
 import { mainMenu } from '../flows/main-menu'
 
-export async function handleCancel(
+export async function handleCancel<T = void>(
     prompt: any | symbol,
-    destination: () => Promise<void> = mainMenu,
+    destination?: () => Promise<T>,
     message: string = 'Operation cancelled.'
-) {
+): Promise<T | undefined> {
     if (ui.prompt.isCancel(prompt)) {
         ui.prompt.cancel(message)
-        return await destination()
+        return (await destination?.()) ?? (mainMenu() as Promise<T>)
     }
 }
