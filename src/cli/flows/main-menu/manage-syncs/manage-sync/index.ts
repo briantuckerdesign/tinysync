2b575b9c6e43a0c8cd7b26454d28b961426a07c4
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-13 12:55:45 -0500

    Create index.ts

---
+++ src/cli/flows/main-menu/manage-syncs/manage-sync/index.ts
@@ -0,0 +1,68 @@
+// import { AsciiTable3 } from 'ascii-table3'
+import { manageSyncs } from '..'
+import { tinySync } from '../../../../../core'
+import type { Sync } from '../../../../../core/types'
+import { ui } from '../../../../ui'
+import { deleteSync } from './delete-sync'
+import { validateSyncTokens } from './validate-sync-tokens'
+import { viewSyncDetails } from './view-sync-details'
+
+export async function manageSync(sync: Sync) {
+    try {
+        // Validate that the sync's tokens still exist in state
+        const tokens = await validateSyncTokens(sync)
+        if (!tokens) return await manageSyncs()
+
+        ui.prompt.log.info(`💎 ${ui.format.bold(sync.name)}`)
+        const syncMessage = 'What would you like to do?'
+        const syncChoices = [
+            {
+                label: 'Sync',
+                value: 'runSync',
+            },
+            {
+                label: 'View details',
+                value: 'viewDetails',
+            },
+            { label: 'Publish site', value: 'publishSite' },
+            { label: 'Delete sync', value: 'deleteSync' },
+            { label: 'Back', value: 'back' },
+            { label: 'Exit', value: 'exit' },
+        ]
+
+        // Asks user what they want to do with the selected sync
+        const userChoice = await ui.prompt.select({
+            message: syncMessage,
+            options: syncChoices,
+        })
+        await ui.handleCancel(userChoice, manageSyncs)
+
+        switch (userChoice) {
+            case 'runSync':
+                await tinySync.sync(sync, tokens.airtable, tokens.webflow)
+                return await manageSync(sync)
+            case 'viewDetails':
+                await viewSyncDetails(sync)
+                break
+            case 'publishSite':
+                await tinySync.publishSite(
+                    tokens.webflow,
+                    sync.config.webflow.site.id
+                )
+                return await manageSync(sync)
+            case 'deleteSync':
+                await deleteSync(sync)
+                break
+            case 'back':
+                return await manageSyncs()
+            case 'exit':
+                ui.prompt.outro('See ya later! 👋')
+                process.exit()
+            default:
+                return await manageSyncs()
+        }
+    } catch (error) {
+        ui.prompt.log.error('Error managing sync.')
+        return await manageSyncs()
+    }
+}
