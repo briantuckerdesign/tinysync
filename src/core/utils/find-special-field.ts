337f0ed8997de755c6ac85bca4b241127c9b68a9
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-15 10:03:29 -0500

    Update find-special-field.ts

--- src/core/utils/find-special-field.ts
+++ src/core/utils/find-special-field.ts
@@ -1,5 +1,30 @@
-import type { Sync } from '../types'
+import type { SpecialField, Sync } from '../types'
 
-export function findSpecial(fieldName: SpecialField, sync: Sync) {
+export function findSpecialField(fieldName: SpecialField, sync: Sync) {
     return sync.fields.find((field) => field.specialField === fieldName)
+}
+
+export function findAllSpecialFields(sync: Sync) {
+    const itemIdField = findSpecialField('itemId', sync)
+    if (!itemIdField) throw new Error('itemIdField not found')
+
+    const stateField = findSpecialField('state', sync)
+    if (!stateField) throw new Error('stateField not found')
+
+    const slugField = findSpecialField('slug', sync)
+    if (!slugField) throw new Error('slugField not found')
+
+    const lastPublishedField = findSpecialField('lastPublished', sync)
+    if (!lastPublishedField) throw new Error('lastPublishedField not found')
+
+    const nameField = findSpecialField('name', sync)
+    if (!nameField) throw new Error('nameField not found')
+
+    return {
+        itemIdField,
+        stateField,
+        slugField,
+        lastPublishedField,
+        nameField,
+    }
 }
