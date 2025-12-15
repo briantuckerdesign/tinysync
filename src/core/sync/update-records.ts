096d9f94912772f9f5aa7f354e082c8ecf7c4b9c
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-15 11:54:13 -0500

    Update update-records.ts

--- src/core/sync/update-records.ts
+++ src/core/sync/update-records.ts
@@ -2,15 +2,17 @@ import { airtable } from '../airtable'
 import type { RecordWithErrors, Sync } from '../types'
 import { findAllSpecialFields } from '../utils/find-special-field'
 import type { CreatedItem } from './create-items'
+import type { UpdatedItem } from './update-items'
 
 export async function updateRecords(
     sync: Sync,
     createdItems: CreatedItem[],
-    failedCreateRecords: RecordWithErrors[],
+    updatedItems: UpdatedItem[],
+    failedRecords: RecordWithErrors[],
     airtableToken: string
 ) {
     const specialFields = findAllSpecialFields(sync)
-
+    /* ------------------------------ create items ------------------------------ */
     for (const createdItem of createdItems) {
         let stateValue = 'Staging'
         switch (
@@ -46,7 +48,43 @@ export async function updateRecords(
         console.log('Updated record:', createdItem.record.id)
     }
 
-    for (const failedRecord of failedCreateRecords) {
+    /* ----------------------------- update records ----------------------------- */
+    for (const createdItem of updatedItems) {
+        let stateValue = 'Staging'
+        switch (
+            createdItem.record.fields[specialFields.stateField.airtable.id]
+        ) {
+            case 'Always sync':
+                stateValue = 'Always sync'
+                break
+            case 'Queued for sync':
+                stateValue = 'Staging'
+                break
+        }
+        const payload = {
+            [specialFields.lastPublishedField.airtable.id]:
+                new Date().toISOString(),
+            [specialFields.slugField.airtable.id]:
+                createdItem.collectionItem.fieldData.slug,
+            [specialFields.stateField.airtable.id]: stateValue,
+        }
+
+        const response = await airtable.update.record(
+            airtableToken,
+            sync.config.airtable.base.id,
+            sync.config.airtable.table.id,
+            createdItem.record.id,
+            payload
+        )
+
+        if (response.error)
+            console.error('Error updating record:', response.error)
+
+        console.log('Updated record:', createdItem.record.id)
+    }
+
+    /* ----------------------------- Failed records ----------------------------- */
+    for (const failedRecord of failedRecords) {
         //convert error array to CSV string
         const errorsAsString = failedRecord.errors.join(', ')
         const payload = {
